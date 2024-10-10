cfe131e2443ddfd4142dc268f5aa5d469ede479f
llooollloo <88551759+llooollloo@users.noreply.github.com>
2024-10-10 15:13:14 +0200

    Create lesson1.cpp

---
+++ lesson1.cpp
@@ -0,0 +1,21 @@
+#include iostream
+using namespace std;
+
+main()
+{
+    cout<<"This is a test"<<endl;
+
+    double number1, number2, number3;
+    double total = number1 + number2 + number3;
+    
+    cout<<"Type a number: "<<endl;
+    cin>>number1;
+    cout<<"Type another one: "<<endl;
+    cin>>number2;
+    cout<<"This is the last one: "<<endl;
+    cin>>number3;
+
+    cout<<"Thanks for the patience, this is the resulting sum: "total;
+    
+    return 0;
+}
